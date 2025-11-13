6702e446de2a28cb09630459aaad31ae22169807
Andres Trujillo <andtruji@c2r2s4.42madrid.com>
2025-11-13 19:58:50 +0100

    Nov 13

--- phil/routine.c
+++ phil/routine.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   routine.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: andrew <andrew@student.42.fr>              +#+  +:+       +#+        */
+/*   By: andtruji <andtruji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 17:55:35 by andtruji          #+#    #+#             */
-/*   Updated: 2025/11/07 09:13:05 by andrew           ###   ########.fr       */
+/*   Updated: 2025/11/13 19:58:22 by andtruji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,27 @@
 
 void	*routine(t_philo *philo)
 {
-	t_rules	*rules;
-
-	rules = philo->rules;
 	if (philo->id % 2 == 0)
-		fake_sleep(2, rules);
+		fake_sleep(2, &philo->rules);
 	while (1)
 	{
-		pthread_mutex_lock(&rules->stop_check);
-		if (rules->stop)
+		pthread_mutex_lock(&philo->rules->stop_check);
+		if (&philo->rules->stop)
 		{
-			pthread_mutex_unlock(&rules->stop_check);
+			pthread_mutex_unlock(&philo->rules->stop_check);
 			break ;
 		}
-		pthread_mutex_unlock(&rules->stop_check);
-		print_state(rules, philo->id, "is thinking");
+		pthread_mutex_unlock(&philo->rules->stop_check);
+		print_state(&philo->rules, philo->id, "is thinking");
 		usleep(300);
 		tk_forks(philo);
 		eat(philo);
 		drop_forks(philo);
-		if (rules->meals_required > 0
-			&& philo->meals_eaten >= rules->meals_required)
+		if (&philo->rules->meals_required > 0
+			&& philo->meals_eaten >= &philo->rules->meals_required)
 			break ;
-		print_state(rules, philo->id, "is slepping");
-		fake_sleep(rules->time_to_sleep, rules);
+		print_state(&philo->rules, philo->id, "is slepping");
+		fake_sleep(&philo->rules->time_to_sleep, &philo->rules);
 	}
 	return (NULL);
 }
